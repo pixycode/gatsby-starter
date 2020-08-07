94f77febf0a935e81bf7a5a1b4f14c5f1821cf1c
Jakub Samsel <41139889+pixycode@users.noreply.github.com>
2020-08-07 10:04:31 +0200

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,6 +10,7 @@ class AboutPage extends Component {
       <Layout>
         <div className="about-container">
           <Helmet title={`About | ${config.siteTitle}`} />
+          <div> Hello world</div>
           <About />
         </div>
       </Layout>
